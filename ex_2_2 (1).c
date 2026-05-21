48c73051b0c4ff8e04072d6f6c69b0f70e204255
ateitelbaum6 <ateitelbaum6@gmail.com>
2026-05-21 08:27:17 +0300

    Add files via upload

---
+++ ex_2_2 (1).c
@@ -0,0 +1,65 @@
+/******************
+Name:
+ID:
+Assignment: ex3
+*******************/
+
+#include <stdio.h>
+
+#define NUM_OF_BRANDS 5
+#define BRANDS_NAMES 15
+#define NUM_OF_TYPES 4
+#define TYPES_NAMES 10
+#define DAYS_IN_YEAR 365
+#define addOne  1
+#define addAll  2  
+#define stats  3
+#define print  4
+#define insights  5
+#define deltas  6
+#define done  7
+
+char brands[NUM_OF_BRANDS][BRANDS_NAMES] = {"Toyoga", "HyunNight", "Mazduh", "FolksVegan", "Key-Yuh"};
+char types[NUM_OF_TYPES][TYPES_NAMES] = {"SUV", "Sedan", "Coupe", "GT"};
+
+
+void printMenu(){
+    printf("Welcome to the Cars Data Cube! What would you like to do?\n"
+           "1.Enter Daily Data For A Brand\n"
+           "2.Populate A Day Of Sales For All Brands\n"
+           "3.Provide Daily Stats\n"
+           "4.Print All Data\n"
+           "5.Provide Overall (simple) Insights\n"
+           "6.Provide Average Delta Metrics\n"
+           "7.exit\n");
+}
+
+
+int main() {
+    int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES];
+    int days[NUM_OF_BRANDS] = {0};
+    int choice;
+    printMenu();
+    scanf("%d", &choice);
+    while(choice != done){
+        switch(choice){
+            case addOne:
+                //...
+                break;
+            case addAll:
+                //...
+                break;
+            /*
+             ......
+             */
+            default:
+                printf("Invalid input\n");
+        }
+        printMenu();
+        scanf("%d", &choice);
+    }
+    printf("Goodbye!\n");
+    return 0;
+}
+
+
